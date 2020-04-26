88b2cf0d24972795f9ba1ed2b568573347c13047
Sebastian Ober <ober.sebastian@googlemail.com>
2020-04-26 16:35:58 +0200

    Day 13 part 2

--- src/main/java/day13/ArcadeComputer.java
+++ src/main/java/day13/ArcadeComputer.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ArcadeComputer extends AdvancedIntcodeComputer {
     Map<Point<Integer>, Tile> screen = new HashMap<>();
@@ -15,8 +16,10 @@ public class ArcadeComputer extends AdvancedIntcodeComputer {
     private int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
     private int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
     private int bufx = 0, bufy = 0;
+    private int score = 0;
     private Point<Integer> ball;
     private Point<Integer> paddle;
+    private static final boolean MANUAL = false;
 
     ArcadeComputer(long[] programme) {
         super(programme, 0);
@@ -24,7 +27,13 @@ public class ArcadeComputer extends AdvancedIntcodeComputer {
 
     @Override
     protected long getInput() {
-        return 0;
+        if(MANUAL) {
+            printScreen();
+            System.out.println("-1 <-- 0 --> 1");
+            Scanner sc = new Scanner(System.in);
+            return sc.nextLong();
+        }
+        return ball.x.compareTo(paddle.x); // -1 if smaller, +1 if larger, 0 if equal
     }
 
     @Override
@@ -38,16 +47,19 @@ public class ArcadeComputer extends AdvancedIntcodeComputer {
             minY = Math.min(minY, bufy);
             maxY = Math.max(maxY, bufy);
         } else {
-            Tile t = Tile.getTile((int)value);
             Point<Integer> p = new Point<>(bufx, bufy);
-            screen.put(p, t);
-            if(t == Tile.BALL) {
-                ball = p;
-            } else if (t == Tile.PADDLE) {
-                paddle = p;
+            if(p.equals(new Point<Integer>(-1,0))) { // scoreboard
+                score = (int)value;
+            } else {
+                Tile t = Tile.getTile((int)value);
+                screen.put(p, t);
+                if(t == Tile.BALL) {
+                    ball = p;
+                } else if (t == Tile.PADDLE) {
+                    paddle = p;
+                }
             }
         }
-
         nextOutputPosition = (nextOutputPosition + 1) % 3;
     }
 
@@ -81,9 +93,15 @@ public class ArcadeComputer extends AdvancedIntcodeComputer {
             return;
         }
 
-        ArcadeComputer ac = new ArcadeComputer(input);
+        ArcadeComputer ac = new ArcadeComputer(input.clone());
         ac.run();
         ac.printScreen();
         System.out.printf("Blocks: %s\n", ac.countTiles(Tile.BLOCK));
+
+        System.out.println("PART 2");
+        input[0] = 2;
+        ac = new ArcadeComputer(input);
+        ac.run();
+        System.out.printf("You got %s points!\n", ac.score);
     }
 }
